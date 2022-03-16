8bf5a2db1987a947b2d47d0b439b1b210e57612a
Dan Gastler <dgastler@gmail.com>
2022-03-16 14:09:02 -0400

    Moved fsbl_Si.py back to main kernel repo.  Fixed comparison mistake

--- Generate_hwInfo.tcl
+++ Generate_hwInfo.tcl
@@ -14,7 +14,7 @@ write_bitstream -force ${apollo_root_path}/bit/top_${build_name}.bit
 #create hwdef file
 write_hwdef -file ${apollo_root_path}/${hw_dir}/top.hwdef -force
 
-if { [expr [package vcompare [version -short] 2019.2 ] >=0] } {
+if { [expr [package vcompare [version -short] 2019.2 ] < 0] } {
     # create the 
     write_sysdef -hwdef ${apollo_root_path}/${hw_dir}/top.hwdef -bit ${apollo_root_path}/bit/top.bit -file ${apollo_root_path}/${hw_dir}/top.hdf -force
 } else { 
