1f1d27907e5a6b044abbe0840e67179323d01c73
Dan Gastler <dgastler@gmail.com>
2022-09-29 16:17:16 -0400

    Removed initial HAL build script

--- HAL_1.tcl
+++
@@ -1,6 +0,0 @@
-source ${BD_PATH}/HAL.tcl
-
-if { [file isfile ${apollo_root_path}/configs/${build_name}/HAL_1.yaml] } {
-    puts "Building HAL layer 1 for ${build_name}"
-    BuildHAL ${apollo_root_path}/configs/${build_name}/HAL_1.yaml
-}
