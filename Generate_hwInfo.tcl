1db0ab10b09920d27dfa7abba35d08e1dedbcc8f
Dan Gastler <dgastler@gmail.com>
2023-08-03 14:55:47 -0400

    Updated the output path for files needed by the kernel

--- Generate_hwInfo.tcl
+++ Generate_hwInfo.tcl
@@ -14,19 +14,19 @@ set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 write_bitstream -force ${apollo_root_path}/bit/top_${build_name}.bit
 
 #create hwdef file
-write_hwdef -file ${apollo_root_path}/${hw_dir}/top.hwdef -force
+write_hwdef -file ${apollo_root_path}/${hw_dir}/${build_name}/top.hwdef -force
 
 if { [expr [package vcompare [clean_version] 2019.2 ] < 0] } {
     # create the 
-    write_sysdef -hwdef ${apollo_root_path}/${hw_dir}/top.hwdef -bit ${apollo_root_path}/bit/top.bit -file ${apollo_root_path}/${hw_dir}/top.hdf -force
+    write_sysdef -hwdef ${apollo_root_path}/${hw_dir}/${build_name}/top.hwdef -bit ${apollo_root_path}/bit/top.bit -file ${apollo_root_path}/${hw_dir}/${build_name}/top.hdf -force
 } else { 
     # create the hwdef file for old builds
-    write_sysdef -hwdef ${apollo_root_path}/${hw_dir}/top.hwdef -bit ${apollo_root_path}/bit/top.bit -file ${apollo_root_path}/${hw_dir}/top -force
+    write_sysdef -hwdef ${apollo_root_path}/${hw_dir}/${build_name}/top.hwdef -bit ${apollo_root_path}/bit/top.bit -file ${apollo_root_path}/${hw_dir}/${build_name}/top -force
 
     #needed for next line since Xilinx is dumb
     open_checkpoint $outputDir/post_route.dcp
     #build Xilinx's new kind of filee....
-    write_hw_platform -fixed -minimal -file ${apollo_root_path}/${hw_dir}/top.xsa -force
+    write_hw_platform -fixed -minimal -file ${apollo_root_path}/${hw_dir}/${build_name}/top.xsa -force
 }
 
 
